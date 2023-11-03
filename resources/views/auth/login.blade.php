a77e3f098976e03750ec8e7ea688601d63c646b0
DanielJoseDolz <ddolz@eurekasoluciones.com.ar>
2023-11-03 10:41:19 -0300

    autocomplete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -141,6 +141,7 @@
                         </div>
                         <div class="input-group mb-3">
                             <input class="form-control" type="password" name="password" placeholder="Contraseña" required autocomplete="off">
+{{--                            por el escaneo de seguridad--}}
                             <div class="input-group-append">
                                 <div class="input-group-text">
                                     <span class="fas fa-lock"></span>
